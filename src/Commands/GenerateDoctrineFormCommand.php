7223fe69eb06ef3cc7fb9dfee72a1f29533bc417
Martin Mika <mail@martinmika.eu>
2016-05-14 09:01:27 +0200

    Added 'arguments' to Doctrine Form generator to simplify input

--- src/Commands/GenerateDoctrineFormCommand.php
+++ src/Commands/GenerateDoctrineFormCommand.php
@@ -5,6 +5,7 @@ namespace Doublemcz\NetteGenerator\Commands;
 use Doublemcz\NetteGenerator\Generators\DoctrineFormGenerator;
 use Doublemcz\NetteGenerator\Generators\DuplicationException;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
@@ -16,7 +17,10 @@ class GenerateDoctrineFormCommand extends Command
 	 */
 	protected function configure()
 	{
-		$this->setName('generate:doctrine-form')->setDescription('Creates form component based on Doctrine entity');
+		$this->setName('generate:doctrine-form')
+			->addArgument('componentName', InputArgument::OPTIONAL, 'Component name (i.e. UserForm)')
+			->addArgument('entityName', InputArgument::OPTIONAL, 'Entity name (i.e. User)')
+			->setDescription('Creates form component based on Doctrine entity');
 	}
 
 	/**
@@ -28,18 +32,24 @@ class GenerateDoctrineFormCommand extends Command
 	{
 		$helper = $this->getHelper('question');
 
-		$question = new Question('Component name: ');
-		do {
-			$componentName = $helper->ask($input, $output, $question);
-		} while (empty($componentName));
+		$componentName = $input->getArgument('componentName');
+		if (empty($componentName)) {
+			$question = new Question('Component name: ');
+			do {
+				$componentName = $helper->ask($input, $output, $question);
+			} while (empty($componentName));
+		}
 
-		$question = new Question('Entity name: ');
-		do {
-			$entityName = $helper->ask($input, $output, $question);
-			if (!class_exists('App\\Entities\\' . $entityName)) {
-				$output->writeln(sprintf("WARNING: Entity %s does not exists.", $entityName));
-			}
-		} while (empty($entityName));
+		$entityName = $input->getArgument('entityName');
+		if (empty($entityName)) {
+			$question = new Question('Entity name: ');
+			do {
+				$entityName = $helper->ask($input, $output, $question);
+				if (!class_exists('App\\Entities\\' . $entityName)) {
+					$output->writeln(sprintf("WARNING: Entity %s does not exists.", $entityName));
+				}
+			} while (empty($entityName));
+		}
 
 		try {
 			$this->getHelper('container')->getByType(DoctrineFormGenerator::class)->generate([
@@ -47,7 +57,7 @@ class GenerateDoctrineFormCommand extends Command
 				'entityName' => $entityName
 			]);
 		} catch (DuplicationException $e) {
-			$output->writeln('ERROR: ' .$e->getMessage());
+			$output->writeln('ERROR: ' . $e->getMessage());
 		}
 
 		return 0;
